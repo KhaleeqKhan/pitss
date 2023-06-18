157728895a9b84a7a241591cfce8207c2f2f8c67
Niklas Mehlem <niklas.mehlem@smail.th-koeln.de>
2023-06-18 17:39:45 +0200

    +Browser Fingerprinting ; Cloaking *jar ind CookieJar unbenannt

--- src/scripts/shopScripts.js
+++ src/scripts/shopScripts.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Hier lang!")
     setTimeout(() => {
         document.getElementById("werbe-link").href = "http://localhost:9081/realsurvey";
     }, 500)
